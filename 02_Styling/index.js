12bcb1001988665f5218689428bdf828f5901ff7
Rafael Mishayev <rafaelmishayev@gmail.com>
2025-11-18 16:47:21 +0200

    new

--- 02_Styling/index.js
+++ 02_Styling/index.js
@@ -1,6 +1,9 @@
 let x = 0;
+
+//Log And Alert
+
 function DoSomthing() {
     x++;
     console.log("Current number is:" + x);
-    alert(`current number is ${x} with alert`);
+    alert(`Current number is ${x} with alert`);
 }
